b610a4f68f7aa4296e63e67025f5336fe4f38eb0
Sanjarbek <sanjarbek@example.com>
2025-04-18 16:21:13 +0500

    modify api.php

--- routes/api.php
+++ routes/api.php
@@ -8,3 +8,5 @@ Route::post('/cart/toggle', [ProductStatusesController::class, 'toggleCart']);
 Route::get('/wishlist', [ProductStatusesController::class, 'wishlist']);
 Route::get('/cart', [ProductStatusesController::class, 'cart']);
 
+
+//bu test uchun duo qiling oxhsain
